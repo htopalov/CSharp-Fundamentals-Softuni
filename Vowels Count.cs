d1c3be81866d5840dd38462b336310b7c5958570
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2020-10-15 17:44:25 +0300

    Add files via upload

---
+++ Vowels Count.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string input = Console.ReadLine().ToLower();
+            int result = VowelsCount(input);
+            Console.WriteLine(result);
+        }
+        static int VowelsCount(string text)
+        {
+            char[] vowelsArr = { 'a', 'e', 'i', 'o', 'u' };
+            int count = 0;
+            foreach (char item in vowelsArr)
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if(item == text[i])
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
