0559a56298782283d4d1afcdae3b4281ec61dd8e
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2020-11-09 09:11:49 +0200

    Create Pirates.cs

---
+++ MyMidExam/Pirates.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Pirates
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int dailyPlunder = int.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+            double totalPlunder = 0;
+            for (int i = 1; i <= days; i++)
+            {
+                totalPlunder += dailyPlunder;
+                if (i % 3 == 0)
+                {
+                    totalPlunder += dailyPlunder * 0.5;
+                }
+                if(i%5==0)
+                {
+                    totalPlunder -= totalPlunder * 0.3;
+                }
+            }
+            double percentage = (totalPlunder/expectedPlunder)*100;
+            if(totalPlunder>= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {totalPlunder:f2} plunder gained.");
+            }
+            else
+            {
+                Console.WriteLine($"Collected only {percentage:f2}% of the plunder.");
+            }
+        }
+    }
+}
