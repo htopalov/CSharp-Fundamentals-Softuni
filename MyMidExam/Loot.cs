0aed9d713f3b234992be9266fab2ac27de6ef3f8
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2020-11-09 09:14:11 +0200

    Add files via upload

---
+++ MyMidExam/Loot.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Loot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> treasureChest = Console.ReadLine()
+                                        .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                                        .ToList();
+            string command = Console.ReadLine();
+            while (command != "Yohoho!")
+            {
+                string[] commandArguments = command.Split().ToArray();
+                string action = commandArguments[0];
+                switch (action)
+                {
+                    case "Loot":
+                        for (int i = 1; i < commandArguments.Length; i++)
+                        {
+                            if (!treasureChest.Contains(commandArguments[i]))
+                            {
+                                    treasureChest.Insert(0, commandArguments[i]);
+                            }
+                        }
+                        break;
+                    case "Drop":
+                        int index = int.Parse(commandArguments[1]);
+                        if(index<0)
+                        {
+                            break;
+                        }
+                        if (index >= 0 && index <= treasureChest.Count)
+                        {
+                            string currentTreasure = treasureChest[index];
+                            treasureChest.RemoveAt(index);
+                            treasureChest.Add(currentTreasure);
+                        }
+                        break;
+                    case "Steal":
+                        int count = int.Parse(commandArguments[1]);
+                        List<string> stolenItems = new List<string>();
+                        if(count <= treasureChest.Count)
+                        {
+                            for (int i = 0; i < count; i++)
+                            {
+                                stolenItems.Add(treasureChest[treasureChest.Count - 1]);
+                                treasureChest.RemoveAt(treasureChest.Count - 1);
+                            }
+                            stolenItems.Reverse();
+                        }
+                        else if(count >=treasureChest.Count)
+                        {
+                            for (int j = 0; j < treasureChest.Count; j++)
+                            {
+                                stolenItems.Add(treasureChest[j]);
+                            }
+                            treasureChest.Clear();
+                        }
+                        
+                        Console.WriteLine(string.Join(", ", stolenItems));
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            int totalStringLength = 0;
+            for (int i = 0; i < treasureChest.Count; i++)
+            {
+                totalStringLength += treasureChest[i].Length;
+            }
+            double averageGain = (double)totalStringLength / treasureChest.Count;
+            if (treasureChest.Count > 0)
+            {
+                Console.WriteLine($"Average treasure gain: {averageGain:f2} pirate credits.");
+            }
+            else if (treasureChest.Count == 0)
+            {
+                Console.WriteLine("Failed treasure hunt.");
+            }
+        }
+    }
+}
---
+++ MyMidExam/Messaging.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> chatCollection = new List<string>();
+            string command = Console.ReadLine();
+            while(command!="end")
+            {
+                string[] commandArguments = command.Split().ToArray();
+                switch(commandArguments[0])
+                {
+                    case "Chat":
+                            chatCollection.Add(commandArguments[1]);
+                        break;
+                    case "Delete":
+                        if(chatCollection.Contains(commandArguments[1]))
+                        {
+                            chatCollection.Remove(commandArguments[1]);
+                        }
+                        break;
+                    case "Edit":
+                        int index = chatCollection.IndexOf(commandArguments[1]);
+                        chatCollection[index] = commandArguments[2];
+                        break;
+                    case "Pin":
+                        int indexPin = chatCollection.IndexOf(commandArguments[1]);
+                        chatCollection.Add(commandArguments[1]);
+                        chatCollection.RemoveAt(indexPin);
+                        break;
+                    case "Spam":
+                            for (int j = 0; j < commandArguments.Length; j++)
+                            {
+                                if (commandArguments[j] != "Spam")
+                                {
+                                        chatCollection.Add(commandArguments[j]);
+                                }
+                            }
+
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            foreach (string word in chatCollection)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
