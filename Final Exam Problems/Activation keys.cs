3b4949a31a0ad586082fe4496e2aed2c172af252
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2020-12-03 21:03:30 +0200

    Add files via upload

---
+++ Final Exam Problems/Activation keys.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Activation_keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rawKey = Console.ReadLine();
+            string command = Console.ReadLine();
+            int startIndex = 0;
+            int endIndex = 0;
+            while (command != "Generate")
+            {
+                string[] arguments = command.Split(">>>", StringSplitOptions.RemoveEmptyEntries);
+                string action = arguments[0];
+                switch (action)
+                {
+                    case "Contains":
+                        string substring = arguments[1];
+                        if (rawKey.Contains(substring))
+                        {
+                            Console.WriteLine($"{rawKey} contains {substring}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Substring not found!");
+                        }
+                        break;
+                    case "Flip":
+                        string subCommand = arguments[1];
+                        startIndex = int.Parse(arguments[2]);
+                        endIndex = int.Parse(arguments[3]);
+
+                        if (subCommand == "Upper")
+                        {
+                            rawKey = rawKey.Substring(0, startIndex) + rawKey.Substring(startIndex, endIndex - startIndex).ToUpper() + rawKey.Substring(endIndex);
+                            Console.WriteLine(rawKey);
+                        }
+                        else if (subCommand == "Lower")
+                        {
+                            rawKey = rawKey.Substring(0, startIndex) + rawKey.Substring(startIndex, endIndex - startIndex).ToLower() + rawKey.Substring(endIndex);
+
+                            Console.WriteLine(rawKey);
+                        }
+                        break;
+                    case "Slice":
+                        startIndex = int.Parse(arguments[1]);
+                        endIndex = int.Parse(arguments[2]);
+                        rawKey = rawKey.Remove(startIndex, endIndex - startIndex);
+                        Console.WriteLine(rawKey);
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Your activation key is: {rawKey}");
+        }
+    }
+}
