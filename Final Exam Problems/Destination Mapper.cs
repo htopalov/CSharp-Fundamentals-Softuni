60fce2a9dfe3af574a132645aa61bfc9496e48e7
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2020-12-06 19:43:33 +0200

    Add files via upload

---
+++ Final Exam Problems/Destination Mapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\=|\/)([A-Z][A-Za-z]{2,})\1";
+            string input = Console.ReadLine();
+            Regex regEx = new Regex(pattern);
+            MatchCollection matchCollection = regEx.Matches(input);
+            int points = 0;
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Destinations: ");
+            foreach (Match item in matchCollection)
+            {
+                points += item.Groups[2].Value.Length;
+                sb.Append(item.Groups[2].Value + ", ");
+            }
+            if (sb.Length>14)
+            {
+                sb.Remove(sb.Length - 2, 2);
+            }
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine($"Travel Points: {points}");
+        }
+    }
+}
---
+++ Final Exam Problems/World Tour.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace World_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string command = Console.ReadLine();
+            while (command != "Travel")
+            {
+                string[] arguments = command.Split(":");
+                string action = arguments[0];
+                switch (action)
+                {
+                    case "Add Stop":
+                        int index = int.Parse(arguments[1]);
+                        string stop = arguments[2];
+                        if (index >= 0 && index < input.Length)
+                        {
+                            input = input.Insert(index, stop);
+                        }
+                        Console.WriteLine(input);
+                        break;
+                    case "Remove Stop":
+                        int startIndex = int.Parse(arguments[1]);
+                        int endIndex = int.Parse(arguments[2]);
+                        if (startIndex >= 0 && startIndex < input.Length && endIndex >= 0 && endIndex < input.Length)
+                        {
+                            input = input.Remove(startIndex, endIndex - startIndex+1);
+                        }
+                        Console.WriteLine(input);
+                        break;
+                    case "Switch":
+                        string oldString = arguments[1];
+                        string newString = arguments[2];
+                        if (input.Contains(oldString))
+                        {
+                            input = input.Replace(oldString, newString);
+                        }
+                        Console.WriteLine(input);
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {input}");
+        }
+    }
+}
